a335bcc35c000bc1ac20f6f398ac19babd9fa999
cappetta <thomas.cappetta@gmail.com>
2017-04-21 21:43:22 -0400

    updates after the hackathon - adding new branch

---
+++ helloworld/tests/all_cli_tests.sh
@@ -0,0 +1,96 @@
+#!/usr/bin/env bash
+
+
+echo "display dispensary information"
+http https://54bsx6qo9l.execute-api.us-east-1.amazonaws.com/dev/dispensary
+
+
+echo "Display OnCall Doctors - Bob"
+http https://54bsx6qo9l.execute-api.us-east-1.amazonaws.com/dev/meet/Bob
+echo "Display OnCall Doctors - Mary"
+http https://54bsx6qo9l.execute-api.us-east-1.amazonaws.com/dev/meet/Mary
+echo "Display OnCall Doctors - Wendel"
+http https://54bsx6qo9l.execute-api.us-east-1.amazonaws.com/dev/meet/Wendel
+echo "Display OnCall Doctors - Doug"
+http https://54bsx6qo9l.execute-api.us-east-1.amazonaws.com/dev/meet/Doug
+
+
+echo "Display Doctors List "
+http https://54bsx6qo9l.execute-api.us-east-1.amazonaws.com/dev/doctors
+
+echo "Display Quality Testing Results - AK-47 "
+http https://54bsx6qo9l.execute-api.us-east-1.amazonaws.com/dev/quality/AK-47
+
+echo "Display Quality Testing Results - BlueLighning "
+http https://54bsx6qo9l.execute-api.us-east-1.amazonaws.com/dev/quality/BlueLighning
+
+echo "Display Quality Testing Results - Gorilla"
+http https://54bsx6qo9l.execute-api.us-east-1.amazonaws.com/dev/quality/Gorilla
+
+
+echo "Display Quality Testing Results - GirlScoutCookies"
+http https://54bsx6qo9l.execute-api.us-east-1.amazonaws.com/dev/quality/GirlScoutCookies
+
+
+echo "Testing Inventory Lookup - Hybrid"
+http https://54bsx6qo9l.execute-api.us-east-1.amazonaws.com/dev/info/hybrid
+
+echo "Testing Inventory Lookup - Sativa"
+http https://54bsx6qo9l.execute-api.us-east-1.amazonaws.com/dev/info/sativa
+
+echo "Testing Inventory Lookup - Indica"
+http https://54bsx6qo9l.execute-api.us-east-1.amazonaws.com/dev/info/indica
+
+#sleep 10
+
+echo "testing place 1st order"
+http https://54bsx6qo9l.execute-api.us-east-1.amazonaws.com/dev/place/1
+
+echo "testing place 2nd order"
+http  https://54bsx6qo9l.execute-api.us-east-1.amazonaws.com/dev/place/2
+
+echo "testing place 3rd order"
+http https://54bsx6qo9l.execute-api.us-east-1.amazonaws.com/dev/place/3
+
+
+#sleep 10
+
+echo "testing retrieve 1st order"
+http https://54bsx6qo9l.execute-api.us-east-1.amazonaws.com/dev/retrieve/1
+
+echo "testing retrieve 2nd order"
+http https://54bsx6qo9l.execute-api.us-east-1.amazonaws.com/dev/retrieve/2
+
+echo "testing retrieve 3rd order"
+http https://54bsx6qo9l.execute-api.us-east-1.amazonaws.com/dev/retrieve/3
+
+
+
+echo "testing register 1st device"
+http https://54bsx6qo9l.execute-api.us-east-1.amazonaws.com/dev/register/Mary
+
+echo "testing register 2nd device"
+http https://54bsx6qo9l.execute-api.us-east-1.amazonaws.com/dev/register/John
+
+echo "testing register 3rd device"
+http https://54bsx6qo9l.execute-api.us-east-1.amazonaws.com/dev/register/Tom
+
+
+echo "testing posting inventory - Mary"
+echo '{"AK-47": "5g"}' |http https://54bsx6qo9l.execute-api.us-east-1.amazonaws.com/dev/add/Mary
+
+echo "testing posting inventory - John"
+echo '{"Blue Lightning": "112g"}' |http https://54bsx6qo9l.execute-api.us-east-1.amazonaws.com/dev/add/John
+
+
+echo "testing posting inventory - Tom"
+echo '{"Cookies": "20", "Cartridges": "10"}' |http https://54bsx6qo9l.execute-api.us-east-1.amazonaws.com/dev/add/Tom
+
+
+
+
+
+echo "Registering phone"
+#todo: add phone number validation logic
+echo '{"me":"5555555"}'| http PUT https://54bsx6qo9l.execute-api.us-east-1.amazonaws.com/dev/sms/5555555
+http  https://54bsx6qo9l.execute-api.us-east-1.amazonaws.com/dev/sms/5555555
---
+++ helloworld/tests/cli_phone_register.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo '{"me":"6319353793"}'| http PUT https://54bsx6qo9l.execute-api.us-east-1.amazonaws.com/dev/sms/6319353793
+http  https://54bsx6qo9l.execute-api.us-east-1.amazonaws.com/dev/sms/me
+
